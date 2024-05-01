c3ed79d897dee73814202af7c74eefd8cad151f4
Simone Silvestroni <me@simonesilvestroni.com>
2024-05-01 16:48:11 +0100

    Added tiny provocative JS script (not inline)

---
+++ assets/js/m2m.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', function() {
+	console.log('404! 🌵 I’m not using any JavaScript here. Want to know why?')
+	console.log('Get in touch: https://minutestomidnight.co.uk/contact/')
+})
