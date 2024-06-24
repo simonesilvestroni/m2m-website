1da23f9d33fbada279362ad1000e60bc25142efe
Simone Silvestroni <hello@simonesilvestroni.com>
2024-06-24 11:55:50 +0100

    Removed fake JS

--- assets/js/m2m.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('load', function() {
-	console.log('404! 🌵 I’m not using any JavaScript here. Want to know why?')
-	console.log('Get in touch: https://minutestomidnight.co.uk/contact/')
-})
